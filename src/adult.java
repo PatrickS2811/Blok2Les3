57e574f953ea316586de449f11e7e6716b1b882f
Patrick Smalen <patricksmalen@MacBook-Pro-van-Patrick.local>
2019-01-30 09:42:10 +0100

    Initial commit

---
+++ src/adult.java
@@ -0,0 +1,9 @@
+public class adult extends Student {
+    private boolean cook;
+    public void setcook(boolean cook) {
+        this.cook = cook;
+    }
+    public boolean getcook() {
+        return cook;
+    }
+}
